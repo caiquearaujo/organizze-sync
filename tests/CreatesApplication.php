b9bee709291b660f273420ab37868aa2a910525a
Caique Araujo <caique@studiopiggly.com.br>
2022-10-23 23:44:31 -0300

    :sparkles: feat(project): Setup of project

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -6,17 +6,15 @@ use Illuminate\Contracts\Console\Kernel;
 
 trait CreatesApplication
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Kernel::class)->bootstrap();
-
-        return $app;
-    }
+	/**
+	 * Creates the application.
+	 *
+	 * @return \Illuminate\Foundation\Application
+	 */
+	public function createApplication()
+	{
+		$app = require __DIR__.'/../bootstrap/app.php';
+		$app->make(Kernel::class)->bootstrap();
+		return $app;
+	}
 }
