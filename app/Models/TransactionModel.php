fbe7894a7e1496303e9d11b72254b46701e4eb3b
Caique Araujo <caique@studiopiggly.com.br>
2022-10-24 09:59:18 -0300

    :sparkles: feat(database): Transactions

---
+++ app/Models/TransactionModel.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Card invoice model.
+ *
+ * @package \App
+ * @subpackage \App\Models
+ * @version 0.1.0
+ * @since 0.1.0
+ * @category Models
+ * @author Caique Araujo <caique@piggly.com.br>
+ * @license MIT
+ * @copyright 2022 Caique Araujo <caique@piggly.com.br>
+ */
+class TransactionModel extends Model
+{
+	/**
+	 * Traits.
+	 */
+	use HasFactory;
+
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 * @since 0.1.0
+	 */
+	protected $table = 'transactions';
+
+	/**
+	 * The primary key associated with the table.
+	 *
+	 * @var string
+	 * @since 0.1.0
+	 */
+	protected $primaryKey = 'local_id';
+
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array
+	 * @since 0.1.0
+	 */
+	protected $casts = [
+		'external_id' => 'integer',
+		'tags' => 'array',
+		'date' => 'date',
+		'amount_cents' => 'integer',
+		'total_installments' => 'integer',
+		'installment' => 'integer',
+		'attachments_count' => 'integer',
+		'paid' => 'boolean',
+		'recurring' => 'boolean',
+		'last_sync' => 'datetime'
+	];
+
+	/**
+	 * Parent.
+	 *
+	 * @return void
+	 * @since 0.1.0
+	 */
+	public function parent()
+	{
+		switch ($this->kind) {
+			case 'account':
+				return $this->belongsTo(CardModel::class, 'account_id', 'local_id');
+			case 'oposite':
+				return $this->belongsTo(AccountModel::class, 'oposite_account_id', 'local_id');
+			case 'card':
+				return $this->belongsTo(CardModel::class, 'card_id', 'local_id');
+			case 'payment':
+				return $this->belongsTo(CardModel::class, 'paid_card_id', 'local_id');
+		}
+
+		return null;
+	}
+}
---
+++ database/migrations/2022_10_24_002420_create_transactions_table.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Card invoices Table.
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ * @category Tables
+ * @author Caique Araujo <caique@piggly.com.br>
+ * @license MIT
+ * @copyright 2022 Caique Araujo <caique@piggly.com.br>
+ */
+class CreateTransactionsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('transactions', function (Blueprint $table) {
+			$table->bigInteger('local_id', true, true)->primary();
+			$table->bigInteger('external_id', false, true)->comment('Organizze external ID')->index();
+
+			$table->bigInteger('category_id', false, true)->nullable();
+			$table->json('tags')->nullable();
+
+			$table->date('date');
+			$table->string('description');
+			$table->text('notes');
+
+			$table->unsignedBigInteger('amount_cents');
+			$table->unsignedTinyInteger('total_installments');
+			$table->unsignedTinyInteger('installment');
+			$table->unsignedTinyInteger('attachments_count');
+
+			$table->enum('kind', ['account', 'card', 'payment', 'oposite']);
+
+			$table->bigInteger('account_id', false, true)->nullable();
+			$table->bigInteger('card_id', false, true)->nullable();
+			$table->bigInteger('card_invoice_id', false, true)->nullable();
+			$table->bigInteger('paid_card_id', false, true)->nullable();
+			$table->bigInteger('paid_card_invoice_id', false, true)->nullable();
+			$table->bigInteger('oposite_transaction_id', false, true)->nullable();
+			$table->bigInteger('oposite_account_id', false, true)->nullable();
+
+			$table->boolean('paid');
+			$table->boolean('recurring');
+
+			$table->timestamp('last_sync')->nullable()->index();
+			$table->timestamps();
+
+			$table->foreign('category_id')->references('local_id')->on('categories')->restrictOnUpdate()->restrictOnDelete();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('transactions');
+	}
+}
